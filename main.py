b03bbfa72f8aae38ca32d237dbec01f53a1d8c67
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-18 17:52:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+
+txt = st.text_area("Medical Abstract", value="",height = 400) # Mostramos el área donde se copia el texto
+pred = st.button("Predict") # Botón para predecir
