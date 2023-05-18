b03b22dd67416b869c551846a3ebaf7ddb704854
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-18 17:58:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-
+# Crear un título
+st.title("¡Bienvenidos a nuestra aplicación web - Movies Predictor!")
 txt = st.text_area("Medical Abstract", value="",height = 400) # Mostramos el área donde se copia el texto
 pred = st.button("Predict") # Botón para predecir
