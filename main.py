02e9aa2e09759640559301055087d0304a28727f
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-18 18:01:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import streamlit as st
 
 # Crear un título
 st.title("¡Bienvenidos a nuestra aplicación web - Movies Predictor!")
-txt = st.text_area("Medical Abstract", value="",height = 400) # Mostramos el área donde se copia el texto
+# Crear una lista de opciones con selección múltiple
+opciones_seleccionadas = st.multiselect("Seleccione los actores", ["Opción 1", "Opción 2", "Opción 3"])
+
 pred = st.button("Predict") # Botón para predecir
