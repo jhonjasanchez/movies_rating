7c47b5a7c389c855e8ffb0e52862dd4ccefd9b4e
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-18 18:02:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 # Crear un título
 st.title("¡Bienvenidos a nuestra aplicación web - Movies Predictor!")
+
+# Crear un campo de texto para el nombre de la película
+nombre_pelicula = st.text_input("Ingrese el nombre de la película:")
+
 # Crear una lista de opciones con selección múltiple
 opciones_seleccionadas = st.multiselect("Seleccione los actores", ["Opción 1", "Opción 2", "Opción 3"])
 
