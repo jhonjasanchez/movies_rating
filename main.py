9c4a657f9caa19ea6d7209f93ae75d77aff3b87f
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-19 10:12:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ def cargar_datos():
   datos=pd.read_csv('DATASET_PELICULAS_SIN_PUNTAJE.csv',sep=";",encoding='latin-1', decimal=',')
   columna = datos['PRIMARYTITLE']
   lista_peliculas = columna.tolist()
-  return datos
+  
+  return lista_peliculas
 
 
 # Carga los datos
-datos = cargar_datos()
+peliculas = cargar_datos()
 
 # Crear un título
 st.title("¡Bienvenidos a nuestra aplicación web - Movies Predictor!")
 
 # Crear un campo de texto para el nombre de la película
 nombre_pelicula = st.text_input("Ingrese el nombre de la película:")
+
+selected_items = st.multiselect("Selecciona elementos", peliculas)
 
 pred = st.button("Predict") # Botón para predecir
