24191b3d55c2b632d66d51db3750419cdaea56cb
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-19 15:49:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ def cargar_datos_peliculas_det():
   datos_peliculas_det=pd.read_csv('DATASET_PELICULAS_FINAL_2023.csv',sep=";",encoding='latin-1', decimal=',')
   return datos_peliculas_det
 
+def consultar_datos_pelicula(tconst):
+  pelicula_filtrada = datos_peliculas_det[datos_peliculas_det['TCONST'] == tconst]
+  return pelicula_filtrada
+
+
 # Carga los datos
 peliculas = cargar_datos()
 peliculas_det = cargar_datos_peliculas_det()
