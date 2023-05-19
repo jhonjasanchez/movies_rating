3e2e62acdf63759ec9da94a62734d9e39500ed5b
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-19 12:15:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,4 +22,16 @@ nombre_pelicula = st.text_input("Ingrese el nombre de la película:")
 
 selected_items = st.selectbox("Selecciona elementos", peliculas)
 
+
+# Obtener la entrada del usuario
+input_usuario = st.text_input("Buscar opción")
+
+# Filtrar las opciones basadas en la entrada del usuario
+opciones_filtradas = [opcion for opcion in peliculas if input_usuario.lower() in opcion.lower()]
+
+# Mostrar las opciones filtradas en el selectbox
+opcion_seleccionada = st.selectbox("Selecciona una opción", opciones_filtradas)
+
+st.write("Opción seleccionada:", opcion_seleccionada)
+
 pred = st.button("Predict") # Botón para predecir
