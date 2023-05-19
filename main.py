60427c109e94884c47a328f1031c9354ee46b515
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-18 20:07:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,4 @@ st.title("¡Bienvenidos a nuestra aplicación web - Movies Predictor!")
 # Crear un campo de texto para el nombre de la película
 nombre_pelicula = st.text_input("Ingrese el nombre de la película:")
 
-# Crear una lista de opciones con selección múltiple
-opciones_seleccionadas = st.multiselect("Seleccione los actores", ["Opción 1", "Opción 2", "Opción 3"])
-
 pred = st.button("Predict") # Botón para predecir
