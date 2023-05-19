dc920a238d0bc5f6456f2a56c3fea7f0b8064f79
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-19 09:18:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-
-# Carga los datos
-datos = cargar_datos()
-
 # Carga tus datos desde una fuente (por ejemplo, un archivo CSV)
 def cargar_datos():
   #datos=pd.read_csv('https://github.com/jhonjasanchez/movies_rating/blob/main/DATASET_PELICULAS_SIN_PUNTAJE.csv',sep=";",encoding='latin-1', decimal=',')
   datos=pd.read_csv('https://github.com/jhonjasanchez/movies_rating/blob/main/DATASET_PELICULAS_SIN_PUNTAJE.csv',sep=";",encoding='latin-1', decimal=',')
   return datos
 
+
+# Carga los datos
+datos = cargar_datos()
 
 # Crear un título
 st.title("¡Bienvenidos a nuestra aplicación web - Movies Predictor!")
