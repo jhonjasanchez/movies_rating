5435db3e8547d253c99b437ac9c2522069b850be
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-18 20:26:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 # Carga tus datos desde una fuente (por ejemplo, un archivo CSV)
 def cargar_datos():
     datos = ''
-    return datos
 
 # Crear un título
 st.title("¡Bienvenidos a nuestra aplicación web - Movies Predictor!")
