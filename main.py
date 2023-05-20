c43210de83ce5ac65f9645913393f71eb27cd717
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-20 15:28:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-
 #Cambiar el tipo de las columnas
 def cambiar_tipos(info_pelicula):
   info_pelicula['startyear'] = info_pelicula['startyear'].astype(str)
@@ -21,6 +20,34 @@ def quitar_columnas(info_pelicula):
          'prom_ponderado_rating_escritores',
          'prom_ponderado_rating_otras_cat']]
   return info_pelicula_2
+
+#Se preparan los géneros de la película
+def preparar_generos(info_pelicula):
+  info_pelicula['genres']=info_pelicula['genres'].str.strip().str.replace(',', ' ')
+  info_pelicula['genres']=info_pelicula.genres.replace({ "\\N": "otro"})
+  unique_values = ['History','War','Comedy','Family','News','Short','Animation','Fantasy',
+                     'Crime','Action','Thriller','Music','Horror','Reality-TV','Documentary',
+                     'Sport','Biography','Adventure','Musical','Adult','Romance','Western',
+                     'otro','Mystery','Drama','Sci-Fi','Talk-Show','Game-Show']
+  for i in unique_values:
+    info_pelicula[i]=np.where(info_pelicula.genres.str.contains(i),1,0)
+  info_pelicula = info_pelicula.drop(['genres'], axis=1)
+  return info_pelicula
+
+# si hay promedios ponderados nulos, se deja con valor de cero (0)
+def preparar_promedios_ponderados(info_pelicula):
+  info_pelicula['prom_ponderado_rating_actores']= np.where(info_pelicula['prom_ponderado_rating_actores'].isna(),0,info_pelicula.prom_ponderado_rating_actores)
+  info_pelicula['prom_ponderado_rating_directores']= np.where(info_pelicula['prom_ponderado_rating_directores'].isna(),0,info_pelicula.prom_ponderado_rating_directores)
+  info_pelicula['prom_ponderado_rating_escritores']= np.where(info_pelicula['prom_ponderado_rating_escritores'].isna(),0,info_pelicula.prom_ponderado_rating_escritores)
+  info_pelicula['prom_ponderado_rating_otras_cat']= np.where(info_pelicula['prom_ponderado_rating_otras_cat'].isna(),0,info_pelicula.prom_ponderado_rating_otras_cat)
+  return info_pelicula
+
+def preparar_pelicula(info_pelicula):
+  info_pelicula = cambiar_tipos(info_pelicula)
+  info_pelicula = quitar_columnas(info_pelicula)
+  info_pelicula = preparar_generos(info_pelicula)
+  info_pelicula = preparar_promedios_ponderados(info_pelicula)
+  return info_pelicula
 
 def clean_column_names(df):
     df.columns = (df.columns.str.strip().str.rstrip().str.lower().str.replace(" ","_").str.replace(".","")
