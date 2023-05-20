5bdea7f9175b4c95a02f70164887bf39ebab94b2
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-20 15:19:36 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -30,10 +30,21 @@ def preparar_generos(info_pelicula):
                      'otro','Mystery','Drama','Sci-Fi','Talk-Show','Game-Show']
   for i in unique_values:
     info_pelicula[i]=np.where(info_pelicula.genres.str.contains(i),1,0)
+  info_pelicula = info_pelicula.drop(['genres'], axis=1)
   return info_pelicula
 
+# si hay promedios ponderados nulos, se deja con valor de cero (0)
+def preparar_promedios_ponderados(info_pelicula):
+  info_pelicula['prom_ponderado_rating_actores']= np.where(info_pelicula['prom_ponderado_rating_actores'].isna(),0,info_pelicula.prom_ponderado_rating_actores)
+  info_pelicula['prom_ponderado_rating_directores']= np.where(info_pelicula['prom_ponderado_rating_directores'].isna(),0,info_pelicula.prom_ponderado_rating_directores)
+  info_pelicula['prom_ponderado_rating_escritores']= np.where(info_pelicula['prom_ponderado_rating_escritores'].isna(),0,info_pelicula.prom_ponderado_rating_escritores)
+  info_pelicula['prom_ponderado_rating_otras_cat']= np.where(info_pelicula['prom_ponderado_rating_otras_cat'].isna(),0,info_pelicula.prom_ponderado_rating_otras_cat)
+  return info_pelicula
 
 def preparar_pelicula(info_pelicula):
   info_pelicula = cambiar_tipos(info_pelicula)
   info_pelicula = quitar_columnas(info_pelicula)
+  info_pelicula = preparar_generos(info_pelicula)
+  info_pelicula = preparar_promedios_ponderados(info_pelicula)
+  
   return info_pelicula
