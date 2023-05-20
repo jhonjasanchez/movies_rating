df7ab7d91afa5d5c92a05e7c443b9ce543357944
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-20 13:44:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ opcion_seleccionada = st.selectbox("Selecciona una película", opciones_filtrada
 
 st.write("Película seleccionada: ", opcion_seleccionada)
 
-if (input_usario != ''):
+if input_usario:
   pelicula_seleccionada = consultar_datos_pelicula(opcion_seleccionada)
   pintar_datos_pelicula_sel(pelicula_seleccionada)
 
