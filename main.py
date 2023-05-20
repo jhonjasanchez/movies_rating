d9e3e6784c6f5059a33e8f36a7d182e091aa370e
jhonjasanchez <jhonjasanchez@gmail.com>
2023-05-20 16:00:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def cambiar_tipos(info_pelicula):
   info_pelicula['startyear'] = info_pelicula['startyear'].astype(str)
   info_pelicula['startyear'] = info_pelicula.startyear.replace({'\\N': np.nan})
   info_pelicula['startyear'] = info_pelicula.startyear.replace({',': ''})
-  info_pelicula['startyear'] = info_pelicula['startyear'].astype(np.float)
+  info_pelicula['startyear'] = info_pelicula['startyear'].astype(np.integer)
   info_pelicula['runtimeminutes'] = info_pelicula['runtimeminutes'].astype(str)
   info_pelicula['runtimeminutes'] = info_pelicula.runtimeminutes.replace({'\\N': np.nan})
   info_pelicula['runtimeminutes'] = info_pelicula['runtimeminutes'].astype(np.float)
